279ba47a2e7df3ce2292f7bfebf9d2cd037ca672
Danila <peace_are_off@bk.ru>
2021-09-20 22:02:21 +0300

    I added arrow for journal images

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,7 +1,8 @@
 const menu_links = document.querySelectorAll(".nav__link");
+
 window.addEventListener('scroll', function () {
     let winScroll = document.documentElement.scrollTop;
     const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     const scrolled = (winScroll / scrollHeight) * 100;
-    document.querySelector(".slider.slider_active").style.width = scrolled + "%";
+    document.querySelector(".menu-slider.menu-slider_active").style.width = scrolled + "%";
 })
